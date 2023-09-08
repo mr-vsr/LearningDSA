65e055fc65017e604742cf0f0c52248fd2564961
MR-VSR <vikasraivsr1234@gmail.com>
2023-09-09 00:48:43 +0530

    pattern package added

--- src/pattern/P1.java
+++ src/pattern/P1.java
@@ -1,2 +1,26 @@
-package pattern;public class P1 {
+package pattern;
+import java.util.Scanner;
+
+
+public class P1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            for (int j = i; j < 2 * i; j++) {
+                System.out.print(j);
+            }
+//           for(int j=1;j<=i;j++){
+//               System.out.print(i+j-1);
+//           }
+            System.out.println();
+        }
+    }
 }
+
+
+//        1234554321
+//        1234**1234
+//        123****123
+//        12******12
+//        1********1
